21507f261b52047b0d9f2e3517756daf54494d85
Pratik Adarsh <pratikadarsh24@gmail.com>
2021-02-02 08:38:57 +0530

    completed BinaryHeap.

--- src/main/python/algorithms/datastructures/priorityqueue/BinaryHeap.py
+++ src/main/python/algorithms/datastructures/priorityqueue/BinaryHeap.py
@@ -1,41 +1,71 @@
+'''Binary Heap implementation using a priority queue.'''
+
 class BinaryHeap():
 
-    def __init__(self):
-        pass
+    def __init__(self, elems=None):
+        heap = list()
 
     def add(self, elem):
-        pass
+        self.heap.append(elem)
+        self.swim(len(self.heap) -1)
 
-    def swim(self, elem):
-        pass
+    def swim(self, elem_index):
+        parent = int((elem_index -1)/2)
+        while(elem_index > 0 and self.heap[elem_index] < self.heap[parent]):
+            swap(elem_index, parent)
+            elem_index = parent
+            parent = int((elem_index-1)/2)
 
-    def sink(self, elem):
-        pass
-    
+    def sink(self, elem_index):
+        while(elem_index >= len(self.heap)):
+            left = 2*elem_index + 1
+            right = 2*elem_index + 2
+            if (left > len(self.heap)) and (right > len(self.heap)):
+                break
+            smallest = left
+            if (right < len(self.heap)-1) and (self.heap[left] >= self.heap[right]):
+                smallest = right
+            swap(elem_index, smallest)
+            elem_index = smallest
+
     def poll(self):
-        pass
+        self.removeAt(0)
 
     def remove(self, elem):
-        pass
+        elem_index = self.contains(elem)
+        if elem_index == -1:
+            raise NotFoundException("Element {} not in heap.".format(elem))
+        self.removeAt(elem_index)
+
 
     def contains(self, elem):
-        pass
+        elem_index = -1
+        for i in range(len(self.heap)):
+            if self.heap[i] == elem:
+                elem_index = i
+                break
+        return elem_index
 
-    def swap(self, elem1, elem2):
-        pass
+    def swap(self, elem1_index, elem2_index):
+        temp = self.heap[elem1_index]
+        self.heap[elem1_index] = self.heap[elem2_index]
+        self.heap[elem1_index] = temp
 
     def peek(self):
-        pass
+        if !isEmpty():
+            return self.heap[0]
 
     def isEmpty(self):
-        pass
+        if len(self.heap) == 0:
+            return True
+        else:
+            return False
 
     def size(self):
-        pass
+        return len(self.heap)
 
     def clear(self):
-        pass
-
+        self.heap = list()
 
 
 
